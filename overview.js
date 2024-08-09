85dfa37d99e8431ee90508b58825ba315e13a015
connorGA <ronnoccap@gmail.com>
2024-08-09 09:48:55 -0700

    overview

---
+++ overview.js
@@ -0,0 +1,34 @@
+// JavaScript:
+/*
+ A high level, interpreted scripting language primarily used for creating interactive and dynamic content on the web.
+ JavaScript allows developers to implement complex features like interactive forms, animations, amnd real-time content updates.
+ It is widely supported across all modern web browsers and can be used on both client side(in the browser) and the server side(using environments like Node.js)
+*/
+
+// TypeScript:
+/*
+ A supersett of JavaScript developed by Microsoft.
+ It adds static typing, classes, and interfaces to JavaScript, enabling better tooling and the ability to catch errors at compile time rather than runtime
+ TypeScript code is transpiled into plain JavaScript, which can then be executed in any JavaScript environment.
+ TypeScript is particularly popular in large-scale projects where maintainability, scalability, and reliability are critical
+
+*/
+
+// Similarities Between JavaScript and TypeScript:
+/*
+1. Syntax: TypeScript shares the same basic syntax as JavaScript, so JavaScript code is valid TypeScript code. Makes it easy for JavaScript devs to transition to TypeScript
+2. Execution Environment: Both TypeScript and JavaScript are ultimately executed as JavaScript in the browser or server(after Typescript is transpiled)
+3. Ecosystem: TypeScript leverages the entire JavaScript ecosystem, meaning you can use all JavaScript libraries and frameworks with TypeScript 
+*/
+
+// Differences Between JavaScript and TypeScript:
+/*
+1. Static Typing: The most significant difference is that TypeScript introduces static types. 
+                  You must declare the types of variables, function parameters, and return values in TypeScript, which helps catch errors at compile time
+2. Type Inference: TypeScript includes type inference, meaning it can often automatically determine the type of variable without explicit annotations, thought you can still provide them
+3. Compile-Time Error Checking: TypeScript checks for errors during the compilation process, which allows developers to catch potential issues before running the code
+4. Advanced Features: TypeScript offers advanced features like interfaces, enums, and generics, which are not available in vanilla JavaScript
+5. Tooling: TypeScript offers better tooling, especially in modern IDEs, due to the type information it provides.
+            This results in features like auto-completion, type checking, and refactoring tools that are more powerful that those available for plain JavaScript
+        
+*/
