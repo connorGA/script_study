79d90d0a7cee2c3a06325c06a847392a572b171d
connorGA <ronnoccap@gmail.com>
2024-08-10 08:56:22 -0700

    Introduction to TypeScript

---
+++ typeScript.ts
@@ -0,0 +1,15 @@
+// 1. Introduction to TypeScript
+/*
+TypeScript Basics:
+    - TypeScript is a typed superset of JavaScript, meaning it includes all JavaScript features but adds optional static types, interfaces, and advanced language features that are absent in vanilla javascript
+Type Annotations:
+    -TypeScript allows you to define the type of a variable, function parameter, or a return value, making your code more predictable and easier to debug
+*/
+// Example:
+let username: string = "John";
+let age: number = 30;
+let isLoggedIn: boolean = true;
+/*
+Compilation:
+    - TypeScript code is compiled into JavaScript, meaning you need to use a TypeScript compiler(e.g,'tsc') to convert .ts files in .js files
+*/
